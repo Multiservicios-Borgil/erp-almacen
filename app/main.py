ce891f6947aade32d04e6c19e027044b451b836f
Multiservicios-Borgil <raulgilcembrano@hotmail.es>
2026-03-19 20:48:07 +0100

    fix: imagen y supabase

--- app/main.py
+++ app/main.py
@@ -19,6 +19,7 @@ import requests
 
 from .database import SessionLocal, engine
 from .models import Base, Item, Familia, Imagen, HistorialDiagnostico
+from supabase_client import supabase
 
 SUPABASE_URL = "https://vmwetkguivvuiehchuax.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtd2V0a2d1aXZ2dWllaGNodWF4Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MjMwNTE1MiwiZXhwIjoyMDg3ODgxMTUyfQ.J1tSVIgoDLOcKD0wj0SFua6UiNJfNH1LAPX3d_DHkPs"
